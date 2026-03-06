2814b4c91dcc80e6a8b491c105161939a4f164a9
Alexandre Bruni Cardoso <brunicar@iq.usp.br>
2026-03-06 10:32:56 -0300

    Update vite config to output to docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/', // Volta para raiz, já que será hospedado em organizacao.github.io
+  build: {
+    outDir: 'docs', // Necessário para o GitHub Pages ler a pasta corretamente
+  }
 })
