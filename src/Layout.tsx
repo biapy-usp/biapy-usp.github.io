2ba1011d11420f075a81a19e7c6a5456545c8f8f
Alexandre Bruni Cardoso <brunicar@iq.usp.br>
2026-03-06 11:15:57 -0300

    Fix logo styling issue

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link, Outlet, useLocation } from 'react-router-dom';
+import './App.css'; // Mudei para garantir que o CSS seja carregado no Layout
 
 export function Layout() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
